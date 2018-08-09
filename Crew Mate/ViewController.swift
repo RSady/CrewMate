e40e3333d3116fe99bdfe9a8f40253323a4d11df
RSady <ryansady@gmail.com>
2018-08-09 15:13:36 -0400

    Initial Commit

---
+++ Crew Mate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crew Mate
+//
+//  Created by Ryan Sady on 8/9/18.
+//  Copyright © 2018 Ryan Sady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
